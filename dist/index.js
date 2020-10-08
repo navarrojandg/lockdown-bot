929328463fdbb6545cc4b24ebcd56468902c7286
jan-home <42248628+navarrojandg@users.noreply.github.com>
2020-10-08 11:13:44 -0700

    feat/ added lockdown handlers

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,82 @@ var discord_js_1 = __importDefault(require("discord.js"));
 var client = new discord_js_1.default.Client();
 var token = process.env.TOKEN;
 client.on('ready', function () {
-    var _a;
-    console.log(((_a = client.user) === null || _a === void 0 ? void 0 : _a.tag) + " is live!");
+    var _a, _b;
+    (_a = client.user) === null || _a === void 0 ? void 0 : _a.setPresence({
+        activity: {
+            type: 'WATCHING',
+            name: '!lockdown'
+        }
+    });
+    console.log(((_b = client.user) === null || _b === void 0 ? void 0 : _b.tag) + " is live!");
 });
+client.on('message', function (msg) {
+    var _a;
+    if (msg.channel.type == 'text') {
+        if ((_a = msg.member) === null || _a === void 0 ? void 0 : _a.roles.highest.permissions.has('ADMINISTRATOR')) {
+            if (msg.content.startsWith('!lockdown'))
+                lockdownHandler(msg);
+        }
+        ;
+    }
+    ;
+    return;
+});
+client.on('messageReactionAdd', function (messageReaction, user) {
+    var _a, _b;
+    if (user.id == ((_a = client.user) === null || _a === void 0 ? void 0 : _a.id))
+        return;
+    var userReaction = (_b = messageReaction.message.guild) === null || _b === void 0 ? void 0 : _b.member(user.id);
+    if (userReaction === null || userReaction === void 0 ? void 0 : userReaction.roles.highest.permissions.has('ADMINISTRATOR')) {
+        if (messageReaction.emoji.name == '🔓')
+            unlockHandler(messageReaction);
+    }
+    ;
+    return;
+});
+var lockdownEmbed = {
+    title: 'Lockdown Started',
+    color: '#FFBC00',
+    description: 'This server is now locked. During this time, `@everyone` will be unable to send messages or connect to voice channels.',
+    fields: [
+        {
+            name: '\u200B',
+            value: 'React with 🔓 to unlock the server.'
+        }
+    ],
+    footer: {
+        text: 'lockdown bot',
+        icon_url: 'https://i.imgur.com/AFN0zRy.png',
+    },
+    timestamp: new Date()
+};
+var unlockEmbed = {
+    title: 'Lockdown Ended',
+    color: '#03FF00',
+    description: 'This server is now unlocked. `@everyone` is now able to send messages or connect to voice channels.',
+    footer: {
+        text: 'lockdown bot',
+        icon_url: 'https://i.imgur.com/AFN0zRy.png',
+    },
+    timestamp: new Date()
+};
+// const lockdownPermissions: Discord.PermissionString[] = [
+//   'SEND_MESSAGES',
+//   'CONNECT',
+//   'STREAM'
+// ];
+function lockdownHandler(msg) {
+    msg.channel.send({ embed: lockdownEmbed })
+        .then(function (m) {
+        m.react('🔓');
+    })
+        .catch(console.error);
+}
+;
+function unlockHandler(messageReaction) {
+    // const everyonePerms = messageReaction.message.guild?.roles.everyone.permissions;
+    // lockdownPermissions.forEach(p => everyonePerms?.add(p));
+    messageReaction.message.channel.send({ embed: unlockEmbed });
+}
+;
 client.login(token);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,82 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import Discord from 'discord.js';
+import Discord, { Message, MessageReaction } from 'discord.js';
 const client = new Discord.Client();
 const token = process.env.TOKEN;
 client.on('ready', () => {
+  client.user?.setPresence({
+    activity: {
+      type: 'WATCHING',
+      name: '!lockdown'
+    }
+  });
   console.log(`${client.user?.tag} is live!`);
 });
+
+client.on('message', (msg: Message) => {
+  if(msg.channel.type == 'text') {
+    if(msg.member?.roles.highest.permissions.has('ADMINISTRATOR')) {
+      if(msg.content.startsWith('!lockdown')) lockdownHandler(msg);
+    };
+  };
+  return;
+});
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if(user.id == client.user?.id) return; 
+  const userReaction = messageReaction.message.guild?.member(user.id);
+  if(userReaction?.roles.highest.permissions.has('ADMINISTRATOR')) {
+    if(messageReaction.emoji.name == '🔓') unlockHandler(messageReaction);
+  };
+  return;
+});
+
+const lockdownEmbed = {
+  title: 'Lockdown Started',
+  color: '#FFBC00',
+  description: 'This server is now locked. During this time, `@everyone` will be unable to send messages or connect to voice channels.',
+  fields: [
+    {
+      name: '\u200B',
+      value: 'React with 🔓 to unlock the server.'
+    }
+  ],
+  footer: {
+    text: 'lockdown bot',
+    icon_url: 'https://i.imgur.com/AFN0zRy.png',
+  },
+  timestamp: new Date()
+};
+
+const unlockEmbed = {
+  title: 'Lockdown Ended',
+  color: '#03FF00',
+  description: 'This server is now unlocked. `@everyone` is now able to send messages or connect to voice channels.',
+  footer: {
+    text: 'lockdown bot',
+    icon_url: 'https://i.imgur.com/AFN0zRy.png',
+  },
+  timestamp: new Date()
+};
+
+// const lockdownPermissions: Discord.PermissionString[] = [
+//   'SEND_MESSAGES',
+//   'CONNECT',
+//   'STREAM'
+// ];
+function lockdownHandler(msg: Message) {
+  msg.channel.send({ embed: lockdownEmbed })
+    .then(m => {
+      m.react('🔓')
+    })
+    .catch(console.error);
+};
+
+function unlockHandler(messageReaction: MessageReaction) {
+  // const everyonePerms = messageReaction.message.guild?.roles.everyone.permissions;
+  // lockdownPermissions.forEach(p => everyonePerms?.add(p));
+  messageReaction.message.channel.send({ embed: unlockEmbed });
+};
 
 client.login(token);
